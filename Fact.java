96a80160a4c62b11a5f0e64a93e301dba5426ea1
Ramyasri135 <kurvaramyasri4242@gmail.com>
2025-01-25 15:50:45 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Fact {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number: ");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }System.out.println("factorial of given number: "+fact);
+    }
+}
